62e117df4b2bacf567527db654d3066073e18bfc
IT21210860 <99806075+IT21210860@users.noreply.github.com>
2022-02-16 13:37:32 +0000

    What did you change?

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  float average , mark01 , mark02 ; //Declaring variables.
+
+  printf("Please input the marks of subject 01 ......\n"); // Display message to the user.
+  scanf("%f", &mark01); // Reads the user input.
+
+  printf("Please input the marks of subject 02 ......\n"); // Display the message to the user.
+  scanf("%f", &mark02); // Reads the user input.
+
+  average = (mark01 + mark02) / 2.00 ; // Average marks calculation.
+
+  printf("Average marks are: %.2f", average); //Display the average.
+
   return 0;
 }
 
